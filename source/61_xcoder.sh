d9f047260cf389186536ff7ed75f1855d710723f
Lex Tang <lexrus@gmail.com>
2015-07-24 13:20:13 +0800

    Remove workaround for Xcode 6.

--- source/61_xcoder.sh
+++ source/61_xcoder.sh
@@ -3,8 +3,8 @@ alias podsync="git -C ~/.cocoapods/repos/master/ remote set-url origin https://g
 alias podinstall="pod install --no-repo-update"
 alias podupdate="pod update --no-repo-update"
 alias ox="open ./*.xcworkspace 2>/dev/null || open ./*.xcodeproj 2>/dev/null"
-alias xcb="sudo xcode-select -s /Applications/Xcode-beta.app/Contents/Developer/"
-alias xc="sudo xcode-select -s /Applications/Xcode.app/Contents/Developer/"
+alias xc7="sudo xcode-select -s /Applications/Xcode-beta.app/Contents/Developer/"
+alias xc6="sudo xcode-select -s /Applications/Xcode.app/Contents/Developer/"
 alias xcp="open $HOME/Library/Application\ Support/Developer/Shared/Xcode/Plug-ins"
 alias ded="rm -rf $HOME/Library/Developer/Xcode/DerivedData"
 alias fucksim="sudo killall -9 Xcode 'iOS Simulator' com.apple.CoreSimulator.CoreSimulatorService && rm -rf ~/Library/Developer/CoreSimulator/Devices"
@@ -18,9 +18,6 @@ alias xcplug="find ~/Library/Application\ Support/Developer/Shared/Xcode/Plug-in
 # Activate fuzzy mode for Xcode 7. See https://twitter.com/hamzasood/status/621739073108111360
 alias xcfuzzy="defaults write com.apple.dt.Xcode IDECodeCompletionFuzzyMode -int 3"
 alias xcfuzzyoff="defaults delete com.apple.dt.Xcode IDECodeCompletionFuzzyMode"
-
-# Workaround for openning Xcode 6.3.2 in El Capitan DP 2
-alias xc6="/Applications/Xcode.app/Contents/MacOS/Xcode </dev/null &>/dev/null &"
 
 # Debugging Xcode Plugins
 # https://coderwall.com/p/-mgtww
